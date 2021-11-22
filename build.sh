a3ff7d29928a2f39cd531e88e1b443bbae629b0c
AdityaGarg8 <85610623+AdityaGarg8@users.noreply.github.com>
2021-11-22 13:38:55 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -79,7 +79,7 @@ do
   fi
   ### Zip iso and split it into multiple parts - github max size of release attachment is 2GB, where ISO is sometimes bigger than that
   cd "${ROOT_PATH}"
-  zip -s 1500m "${ROOT_PATH}/output/livecd-${KERNEL_VERSION}-${ALTERNATIVE}.zip" "${ROOT_PATH}/ubuntu-20.04-${KERNEL_VERSION}-${ALTERNATIVE}.iso"
+  zip -s 1500m "${ROOT_PATH}/output/livecd-${KERNEL_VERSION}-${ALTERNATIVE}.zip" "${ROOT_PATH}/ubuntu-21.10-${KERNEL_VERSION}-${ALTERNATIVE}.iso"
 done
 ## Calculate sha256 sums of built ISO
 sha256sum "${ROOT_PATH}"/*.iso >"${ROOT_PATH}/output/sha256"
