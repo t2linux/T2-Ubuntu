ca06afa89e78a74feb6e176a08b0e95ce5803d29
Aditya Garg <85610623+AdityaGarg8@users.noreply.github.com>
2023-01-28 16:16:19 +0530

    Remove suspend fix

--- 01_build_file_system.sh
+++ 01_build_file_system.sh
@@ -35,8 +35,3 @@ umount "${CHROOT_PATH}/dev"
 echo >&2 "===]> Info: Patch Grub... "
 cp -rfv "${ROOT_PATH}"/files/grub/30_os-prober "${CHROOT_PATH}"/etc/grub.d/30_os-prober
 chmod 755 "${CHROOT_PATH}"/etc/grub.d/30_os-prober
-
-### Copy suspend fix
-echo >&2 "===]> Info: Fix suspend... "
-cp -rfv "${ROOT_PATH}"/files/suspend/rmmod_tb.sh "${CHROOT_PATH}"/lib/systemd/system-sleep/rmmod_tb.sh
-chmod +x "${CHROOT_PATH}"/lib/systemd/system-sleep/rmmod_tb.sh
