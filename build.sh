13211888a2405a480d47843fe95469c65d658d31
Aditya Garg <85610623+AdityaGarg8@users.noreply.github.com>
2023-02-23 01:43:41 +0530

    6.1.13

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ ROOT_PATH=$(pwd)
 WORKING_PATH=/root/work
 CHROOT_PATH="${WORKING_PATH}/chroot"
 IMAGE_PATH="${WORKING_PATH}/image"
-KERNEL_VERSION=6.1.11
+KERNEL_VERSION=6.1.13
 PKGREL=1
 sed -i "s/KVER/${KERNEL_VERSION}/g" $(pwd)/files/chroot_build.sh
 sed -i "s/PREL/${PKGREL}/g" $(pwd)/files/chroot_build.sh
