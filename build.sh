93668650fb283c0d1cabf3ba342719d9daa1e6ad
Aditya Garg <85610623+AdityaGarg8@users.noreply.github.com>
2023-04-30 18:42:53 +0530

    23.04 - Lunar Lobster

--- build.sh
+++ build.sh
@@ -36,11 +36,11 @@ apt-get install -y -qq -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="
   syslinux
 
 echo >&2 "===]> Info: Start loop... "
-for ALTERNATIVE in t2-kinetic
+for ALTERNATIVE in t2-lunar
 do
   echo >&2 "===]> Info: Start building ${ALTERNATIVE}... "
 
-  echo >&2 "===]> Info: Build Ubuntu Kinetic... "
+  echo >&2 "===]> Info: Build Ubuntu Lunar... "
   /bin/bash -c "
     ROOT_PATH=${ROOT_PATH} \\
     WORKING_PATH=${WORKING_PATH} \\
@@ -51,7 +51,7 @@ do
     ${ROOT_PATH}/01_build_file_system.sh
   "
 
-  echo >&2 "===]> Info: Build Image Kinetic... "
+  echo >&2 "===]> Info: Build Image Lunar... "
   /bin/bash -c "
     ROOT_PATH=${ROOT_PATH} \\
     WORKING_PATH=${WORKING_PATH} \\
@@ -85,7 +85,7 @@ do
   fi
   ### Zip iso and split it into multiple parts - github max size of release attachment is 2GB, where ISO is sometimes bigger than that
   cd "${ROOT_PATH}"
-  zip -s 1500m "${ROOT_PATH}/output/livecd-${KERNEL_VERSION}-${ALTERNATIVE}.zip" "${ROOT_PATH}/ubuntu-22.10-${KERNEL_VERSION}-${ALTERNATIVE}.iso"
+  zip -s 1500m "${ROOT_PATH}/output/livecd-${KERNEL_VERSION}-${ALTERNATIVE}.zip" "${ROOT_PATH}/ubuntu-23.04-${KERNEL_VERSION}-${ALTERNATIVE}.iso"
 done
 ## Calculate sha256 sums of built ISO
 sha256sum "${ROOT_PATH}"/*.iso >"${ROOT_PATH}/output/sha256"
