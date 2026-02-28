2cda898c380a2bf21e45d19bb8e698ff1247f09b
MilkshakesZA <dylan.auton@gmail.com>
2026-02-28 19:59:10 +0200

    chnaged curl to append, fixed version inputting

--- .github/workflows/iso.bat
+++ .github/workflows/iso.bat
@@ -15,8 +15,7 @@ ECHO 1. Ubuntu
 ECHO 2. Kubuntu 
 ECHO 3. Ubuntu Unity 
 ECHO.
-SET /P flavinput=Type your choice (1, 2 etc.) from the above list and press return.:
-::todo fix milti line string in this ass language
+SET /P flavinput=Type your choice (1, 2 etc.) from the above list and press return.
 
 IF "%flavinput%"=="1" (
     SET flavour=ubuntu
@@ -40,13 +39,12 @@ ECHO 1. 24.04 LTS - Noble Numbat
 ECHO 2. 25.10 - Questing Quokka 
 ECHO.
 SET /P verinput=Type your choice (1 or 2) from the above list and press return.
-::todo fix milti line string in this ass language
 
-IF "%flavinput%"=="1" (
+IF "%verinput%"=="1" (
     SET iso=%flavour%-24.04-%latestkver%-t2-noble
     SET ver=24.04 LTS - Noble Numbat
 ) ELSE (
-    IF "%flavinput%"=="2" (
+    IF "%verinput%"=="2" (
         SET iso=%flavour%-25.10-%latestkver%-t2-questing
         SET ver=25.10 - Questing Quokka 
     ) ELSE (
@@ -57,13 +55,13 @@ IF "%flavinput%"=="1" (
 )
 
 ECHO Downloading Part 1 for %flavour% %ver%
-curl -A "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64)" -L https://github.com/t2linux/T2-Ubuntu/releases/download/%latest%/%iso%.iso.00 -o %downloads%\0%iso%.iso
+curl -A "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64)" -L https://github.com/t2linux/T2-Ubuntu/releases/download/%latest%/%iso%.iso.00 >> %downloads%\%iso%.iso
 
 ECHO Downloading Part 2 for %flavour% %ver%
-curl -A "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64)" -L https://github.com/t2linux/T2-Ubuntu/releases/download/%latest%/%iso%.iso.01 -o %downloads%\1%iso%.iso
+curl -A "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64)" -L https://github.com/t2linux/T2-Ubuntu/releases/download/%latest%/%iso%.iso.01 >> %downloads%\%iso%.iso
 
 ECHO Downloading Part 3 for %flavour% %ver%
-curl -A "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64)" -L https://github.com/t2linux/T2-Ubuntu/releases/download/%latest%/%iso%.iso.02 -o %downloads%\2%iso%.iso
+curl -A "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64)" -L https://github.com/t2linux/T2-Ubuntu/releases/download/%latest%/%iso%.iso.02 >> %downloads%\%iso%.iso
 
 FOR /f "tokens=1,2 delims= " %%a IN ("%ver%") DO (
   SET shortver=%%a
@@ -82,16 +80,6 @@ DEL shafile.txt
 FOR /f "tokens=1,2 delims= " %%a IN ("%actual_iso_chksum%") DO (
   SET actual_iso_chksum=%%a
 )
-
-ECHO combining parts
-
-COPY /B %downloads%\0%iso%.iso + %downloads%\1%iso%.iso + %downloads%\2%iso%.iso %downloads%\%iso%.iso
-
-ECHO cleaning up
-
-DEL %downloads%\0%iso%.iso
-DEL %downloads%\1%iso%.iso
-DEL %downloads%\2%iso%.iso
 
 ECHO Verifying sha256 checksums
 
